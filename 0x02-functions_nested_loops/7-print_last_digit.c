7072c6067cd997e4f4a207c0ed494f87a9117886
Khaikhai01 <shishchei@gmail.com>
2022-09-14 10:59:45 -0700

    fixed error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,11 @@
 
 /**
 * print_last_digit - function that prints the last digit of a number.
- @n: prints the numbe in question.
+* @n: prints the number in question.
 *
 * Return: value of the last digit.
 */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
