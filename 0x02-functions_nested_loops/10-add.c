8e6d2613eb1cf09336e7add37a9e11b50203784e
Khaikhai01 <shishchei@gmail.com>
2022-09-14 12:07:31 -0700

    battery street, the OG

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
 * add - Adds two integers.
-* @num1 - The first integer to be added.
-* @num2 - The second integer to be added.
+* @num1 - first integer of the addition.
+* @num2 - second integer of the addition.
 *
 * Return: The result of the addition.
 */
