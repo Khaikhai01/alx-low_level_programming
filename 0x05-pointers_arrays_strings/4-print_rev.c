2f898a989110b1bbcc87b6df586b885c2ecad9c4
Khaikhai01 <shishchei@gmail.com>
2022-09-20 11:54:07 -0700

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,13 @@
 */
 void print_rev(char *s)
 {
-	int len = strlen(s);
+	int len = 0, index;
 
-	while (len--)
-		putchar(*(s + len));
-	putchar(10);
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
 }
