b37a431674a606997ea123bf83ee8e6f4db052f6
Khaikhai01 <shishchei@gmail.com>
2022-09-27 22:04:48 +0300

    task 4 betty issues

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,18 +2,18 @@
 #include <string.h>
 
 /**
- *_strpbrk -  locates the first occurrence in the
- *string s of any of the bytes in the string accept.
+ *_strpbrk - searchs a string for any of a set of bytes
  *@s: The null-terminated string to be scanned
  *@accept: The null-terminated string containing the characters to match.
  *
- @Return: a pointer to the byte in s that matches one of
- *the bytes in accept, or NULL if no such byte is found.
+ *Return: a pointer to the byte in @s that matches one of
+ *the bytes in @accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *ptr = strpbrk (s, accept);
-	if(ptr != NULL)
+	char *ptr = strpbrk(s, accept);
+
+	if (ptr != NULL)
     	{	
         	return (ptr);
 	}
@@ -21,5 +21,5 @@ char *_strpbrk(char *s, char *accept)
     	{
         	return (NULL);
     	}
-    	return 0;
+    	return (0);
 }
