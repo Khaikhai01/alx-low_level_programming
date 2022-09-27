d0e010b54a2f1de1c3696178c34f5db8a93b8f94
Khaikhai01 <shishchei@gmail.com>
2022-09-27 21:42:59 +0300

    code fail fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,12 +4,12 @@
  *_strspn - gets length of a substring
  *@s: string to be scanned.
  *@accept: string containing the characters to match.
- *Return: number of characters in s which consist only of 
- *characters from accept.
+ *Return: number of characters in s which consist only of
+ *characters from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 	int c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
