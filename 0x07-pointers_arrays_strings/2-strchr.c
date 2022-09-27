1dc0b7ff1c330684b94b08672fcde3621ac32668
Khaikhai01 <shishchei@gmail.com>
2022-09-27 07:36:51 +0300

    commited files

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strchr - locates a character in a string
+ *@s: c string.
+ *@c: character to be located.
+ *Return: a pointer to the first occurence oc c in s, if character
+ *is not found, te function returns null pointer.
+ */
+char *_strchr(char *s, char c)
+{
+	
+}
