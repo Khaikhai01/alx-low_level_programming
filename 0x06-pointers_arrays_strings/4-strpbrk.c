5d46aab3c6e693a59f435f80ba2ddc0eb175ee59
Khaikhai01 <shishchei@gmail.com>
2022-09-27 13:47:03 -0700

    new changes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *_strpbrk - searchs a string for any of a set of bytes
+ *@s: The null-terminated string to be scanned
+ *@accept: The null-terminated string containing the characters to match.
+ *
+ *Return: a pointer to the byte in @s that matches one of
+ *the bytes in @accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr = strpbrk(s, accept);
+
+	if (ptr != NULL)
+	{
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (0);
+}
