da3a73e0fe92d6b7c7f61b706f2a8fb88506d169
Khaikhai01 <shishchei@gmail.com>
2022-09-27 21:33:20 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	while (*s)
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*accept != *s)
+		if (s[i] != 32)
 		{
-			s++;
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
 		}
 		else
-			putchar(*accept);
-	}
-	if (*accept == '\0')
-	{
-		return (*s);
+			return (c);
 
 	}
-	return (0);
+	return (c);
 }
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strpbrk -  locates the first occurrence in the
+ *string s of any of the bytes in the string accept.
+ *@s: The null-terminated string to be scanned
+ *@accept: The null-terminated string containing the characters to match.
+ *
+ @Return: a pointer to the byte in s that matches one of
+ *the bytes in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr = strpbrk (s, accept);
+    if(ptr != NULL)
+    {
+        return (ptr);
+    }
+    else
+    {
+        return (NULL);
+    }
+    return 0;
+}
