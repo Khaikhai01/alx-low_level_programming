f61a5e81ed6407db21326691172a158ddc8b8405
Khaikhai01 <shishchei@gmail.com>
2022-09-28 14:52:21 +0300

    string reverse and length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ *_strlen_recursion - function that returns the lenght of a string.
+ *@s: string whose length is to be calculated
+ *Return: the length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
