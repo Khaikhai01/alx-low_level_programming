533edfe023acc9ed6156e8d56ae28a27e53a4cd9
Khaikhai01 <shishchei@gmail.com>
2022-09-29 06:31:45 +0300

    power recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,17 +8,14 @@
 
 int is_prime_number(int n)
 {
-	int i, c = 0;
+	int i;
 
-	for (i = 1; i <= n; i++)
-		if (n % i == 0)
-		{
-			c++;
-		}
-	if (c == 2)
+	if (n <= 1)
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
 }
