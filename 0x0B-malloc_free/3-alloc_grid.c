fc648d58a7b335170df00e5b0b92c4d8ad6082a2
Khaikhai01 <shishchei@gmail.com>
2022-10-05 05:35:00 +0300

    betty styling

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *alloc_grid - function that returns a pointer to a
- *2 dimensional array of integers.
+ *alloc_grid - 2 dimensional array of integers.
  *@width: width paramenter
  *@height: height parameter
  *Retrun: NULL on failure
