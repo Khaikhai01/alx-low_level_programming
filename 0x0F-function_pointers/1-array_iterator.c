930115f8ab1d50c510f6bf2aea339ca60ab00e65
Khaikhai01 <shishchei@gmail.com>
2022-10-14 19:56:47 +0300

    commenting issue

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  *array_iterator - executes a function given as a parameter
  *on each element of an array.
  *@array: array pointer
- *@size - size of the array
+ *@size: size of the array
  *@action: pointer to the function
  */
 
