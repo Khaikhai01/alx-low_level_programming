13f6a511d59681e8e4897e3540f3f2be9cefa68f
Khaikhai01 <shishchei@gmail.com>
2022-10-27 13:05:55 +0300

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function that frees a listint_t list.
+ *@head: head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_head;
+
+	while (*head)
+	{
+		if (*head == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			free_head = *head;
+			*head = (*head)->next;
+			free(free_head);
+		}
+	}
+}
