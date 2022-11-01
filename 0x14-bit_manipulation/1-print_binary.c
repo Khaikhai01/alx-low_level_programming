31ba8b3a7cc6290ab961df011f8e2d815e50eb46
Khaikhai01 <shishchei@gmail.com>
2022-11-01 16:54:48 +0300

    updated file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,13 +6,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n < 2)
+	if (n > 1)
 	{
-		_putchar(n + '0');
-		return;
+		print_binary(n >> 1);
 	}
-
-	print_binary(n >> 1);
-
-	_putchar((n & 1) + '0');
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
