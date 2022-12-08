e3ba189107d1d4a2bf680961da012df87188d320
Khaikhai01 <shishchei@gmail.com>
2022-12-08 21:21:00 +0300

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	}
 
-	while (temp && n!= index)
+	while (temp && n != index)
 	{
 		n++;
 		temp = temp->next;
